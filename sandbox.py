9cfe84c573fa87d6f06417b50ae4628045805cc1
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2023-09-21 16:19:50 -0400

    finished and uploaded

--- sandbox.py
+++ sandbox.py
@@ -1,13 +1,1 @@
-# Create a method for your Pokemon class that takes in user input, 
-# to ask what new pokemon your current pokemon should evolve into.
-# Create a method
-# Take in user input, should be a pokemons name
-# Update the current pokemon's attributes (their height, weight, sprite, etc.) to the new pokemon's values
 
-pikachu = Pokemon('pikachu')
-
-print(pikachu.sprite)
-
-pikachu.evolve()
-
-pikachu.sprite
